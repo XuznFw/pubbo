bc9f4b9d5442886fd5b5aa85955ecc12c70506d5
xuzn <xuzn@fshows.com>
2020-04-27 15:00:58 +0800

    补全hessian协议

--- pubbo/serialization.py
+++ pubbo/serialization.py
@@ -1,7 +1,7 @@
 import random
 import json
-import dubbo.hessian
-import dubbo.common
+from .common import FlagEnum, RequestMessage, ResponseMessage, ResponseTypeEnum
+from .hessian import Hessian2
 
 
 class Serialization(object):
@@ -9,20 +9,12 @@ class Serialization(object):
     magic_low: bytes = 0xbb.to_bytes(length=1, byteorder="big")
     flag: bin = None
     two_way: bin = None
-    event: bin = 0b00000000
+    event: bin = None
     serialization_id: bin = None
-    status: bytes = 0b00000000.to_bytes(length=1, byteorder="big")
+    status: bytes = None
     request_id: bytes = None
     data_length: bytes = None
     variable_part: bytes = None
-
-    __flag_request = 0b10000000
-    __flag_response = 0b00000000
-
-    def __init__(self):
-        self.request_id = random.randint(0, 4294967295).to_bytes(length=8, byteorder="big")
-        self.flag = self.__flag_request
-        self.two_way = 0b01000000
 
     @property
     def magic(self):
@@ -32,14 +24,36 @@ class Serialization(object):
     def message(self):
         second_position_bin = self.flag | self.two_way | self.event | self.serialization_id
         second_position = second_position_bin.to_bytes(length=1, byteorder="big")
-
         return self.magic + second_position + self.status + self.request_id + self.data_length + self.variable_part
 
     def encode(self, message):
-        raise Exception()
+        if isinstance(message, (RequestMessage,)):
+            return self.encode_request(message)
+        elif isinstance(message, (ResponseMessage,)):
+            raise Exception("todo")
+        else:
+            raise Exception("oop")
 
-    def decode(self, message):
-        serialization: Serialization = None
+    def decode(self, flag: FlagEnum, message):
+        if flag is FlagEnum.REQUEST:
+            raise Exception("todo")
+        elif flag is FlagEnum.RESPONSE:
+            return self.decode_response(message)
+        else:
+            raise Exception("oop")
+
+    def init_request(self):
+        self.flag = FlagEnum.REQUEST.value
+        self.two_way = 0b01000000
+        self.event = 0b00000000
+        self.status = 0b00000000.to_bytes(length=1, byteorder="big")
+        self.request_id = random.randint(0, 4294967295).to_bytes(length=8, byteorder="big")
+
+    def encode_request(self, message):
+        raise Exception("need to overwrite")
+
+    def decode_response(self, message):
+        serialization = None
         for clazz in Serialization.__subclasses__():
             if clazz.serialization_id == message[2]:
                 serialization = clazz()
@@ -52,13 +66,31 @@ class Serialization(object):
         serialization.request_id = message[4:12]
         serialization.data_length = message[12:16]
         serialization.variable_part = message[16:16 + int.from_bytes(serialization.data_length, byteorder='big')]
-        return serialization.decode(message)
+        return serialization.decode_response(message)
+
+
+class HessianSerialization(Serialization):
+    serialization_id = 0b00000010  # 2
+
+    def decode_response(self, message):
+        response = ResponseMessage()
+
+        response_type = ResponseTypeEnum.response_type(Hessian2(self.variable_part[0:1]).decode())
+        response.type = response_type
+
+        if response.type is ResponseTypeEnum.NULL:
+            response.message = None
+            return response
+
+        response.message = Hessian2(self.variable_part[1:]).decode()
+        return response
 
 
 class FastJSONSerialization(Serialization):
     serialization_id = 0b00000110  # 6
 
-    def encode(self, message):
+    def encode_request(self, message):
+        self.init_request()
         attachments = {
             "path": message.service_name,
             "interface": message.service_name,
@@ -78,21 +110,4 @@ class FastJSONSerialization(Serialization):
         ]
         self.variable_part = ("\r\n".join(message_list) + "\r\n").encode("utf-8")
         self.data_length = len(self.variable_part).to_bytes(length=4, byteorder="big")
-
-
-class HessianSerialization(Serialization):
-    serialization_id = 0b00000010  # 2
-
-    def decode(self, message):
-        response = dubbo.common.ResponseMessage()
-
-        response.type = dubbo.hessian.Hessian2(self.variable_part[0:1]).decode()
-
-        if response.type == 0:
-            exception = dubbo.hessian.Hessian2(self.variable_part[1:]).decode()
-            return dubbo.common.GenericException(exception)
-        elif response.type == 1:
-            result = dubbo.hessian.Hessian2(self.variable_part[1:]).decode()
-            return dubbo.common.Response(result)
-        elif response.type == 2:
-            return None
+        return self.message
